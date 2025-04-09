1521da9bfd8fbcce1d5b855b9756544ac46ed113
Seamus Herrod <seamusmherrod@ku.edu>
2025-04-09 18:18:55 -0500

    retreiving top level function will be used for further analysis

--- Ast_Analysis/src/analysis.rs
+++ Ast_Analysis/src/analysis.rs
@@ -1,5 +1,7 @@
 use std::fs;
+use std::io;
 use syn::{parse_file, File};
+
 
 pub fn get_ast(file_path: String) -> File {
     let source = fs::read_to_string(file_path).expect("Failed to read file path");
@@ -7,6 +9,33 @@ pub fn get_ast(file_path: String) -> File {
     ast
 }
 
-pub fn gen_path(ast: File) {
+// call this for top-level function you would like to analyze 
+pub fn get_fn_item(ast: File) -> Option<syn::Item> {
+    /*-----------------------------------------
+        Steps: 
+            iterate through the ast 
+                extract the function which should be analyzed's node (not necessarily a main function)  
+    -----------------------------------------*/
+    println!("Enter Function to be Analyzed: ");
+    let mut function_to_analyze: String = String::new(); // enter top level fn to analyze
+    io::stdin().read_line(&mut function_to_analyze).expect("Failed to read line");
+
+    for item in ast.items {
+        if let syn::Item::Fn(func) = item {
+            if func.sig.ident.to_string() == function_to_analyze.trim() {
+                return Some(syn::Item::Fn(func));
+            }
+        }
+    }
+    // if we exit the loop w/o return an ItemFn we can return None
+    None
+}
+
+pub fn get_path(fn_item: syn::Item) -> Vec<syn::Item> {
+    /*-----------------------------------------
+        step through the blocks w/in the top function,
+            when you encounter: fn, or stmt (conditional)
+                add that block to the returned vector
+    -----------------------------------------/*
     
 }
--- Ast_Analysis/src/main.rs
+++ Ast_Analysis/src/main.rs
@@ -4,5 +4,7 @@ mod analysis;
 fn main() {
     let file_path: String = "../../POC_simple/src/simple_2.rs".to_string();
     let ast: File = analysis::get_ast(file_path);
-    println!("{:#?}", ast);
+    let top_level_fn_item: syn::Item = analysis::get_fn_item(ast)
+                            .expect("Could not find top-level function for analysis");
+    dbg!(top_level_fn_item);
 }
