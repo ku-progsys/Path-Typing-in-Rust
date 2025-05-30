2ccb3ae8b986d2b8c175355701e647b0b50598d3
Seamus Herrod <seamusmherrod@gmail.com>
2025-05-30 14:47:10 -0500

    minor changes

--- POC_simple/src/simple_1.rs
+++ POC_simple/src/simple_1.rs
@@ -2,7 +2,7 @@
 pub fn run_simple1(flag: bool) {
     let mut usr_data: String = get_usr_data();
     let data: i32 = 12;
-    if flag == true && data == 12 { // make implicit example similar 
+    if flag == true && data == 12{ // make implicit example similar 
         process_data(&usr_data);
     }
     else {
